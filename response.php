2f9e699823b763d27238f3187a71d8e2256e5c9c
Darien Pourmehr <darienpourmehr@gmail.com>
2015-10-17 23:47:22 -0400

    added header

--- response.php
+++ response.php
@@ -1,9 +1,17 @@
 <?php
 
+ 
+if(isset($_POST['username']) {
+	$dbhost = 'ealybe55bg.database.windows.net';
+	$dbuser = 'opallity';
+	$dbpass = 'Iltemp071993';
 
-if (isset($_POST["userName"]) && isset($_POST["password"]) && !empty($_POST["userName"]) && !empty($_POST["password"])) {
-    echo "Yes, user and pass is set";    
+	$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die                      ('Error connecting to mysql');
+
+	$dbname = 'OpallityFinal2';
+	mysql_select_db($dbname);
+
+	echo 'yes';
 }
-
 
 ?>
