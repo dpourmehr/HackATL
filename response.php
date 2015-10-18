0d608cdb09b7ca8714c3c88c9a7f9f18370c6e56
Darien Pourmehr <darienpourmehr@gmail.com>
2015-10-18 05:11:59 -0400

    added db

--- response.php
+++ response.php
@@ -16,7 +16,7 @@ $results = array();
 while($row = mysqli_fetch_assoc($sqlResult)) {
 	$results[] = $row;
 }
-echo json_encode($results);
+print json_encode($results);
 
 /*if ($conn->query("INSERT INTO Players (username, password, firstName, lastName, email, zipcode) VALUES ('rsmith', '1111', 'Robert', 'Smith', 'frogger287@gmail.com', '30092')") === TRUE) {
     echo json_encode("Rob was entered into the database correctly.");
