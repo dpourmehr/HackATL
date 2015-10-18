b6790174bef66418657ccfd137c07987317c361e
Darien Pourmehr <darienpourmehr@gmail.com>
2015-10-18 04:48:43 -0400

    added db

--- response.php
+++ response.php
@@ -10,6 +10,6 @@ $conn = mysqli_connect($host, $user, $pwd);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-$result = mysqli_query($conn,"SELECT * FROM Players");
+$result = mysqli_query($conn,"CREATE TABLE Players (username varchar(255), password varchar(255), firstName varchar(255), lastName varchar(255), email varchar(255), zipcode varchar(255))");
 echo json_encode($result);
 ?>
