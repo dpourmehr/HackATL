b9469937b67c0fdfc8ed2ea35952e47611e60cd1
Darien Pourmehr <darienpourmehr@gmail.com>
2015-10-18 02:02:53 -0400

    added header

--- response.php
+++ response.php
@@ -1,14 +1,22 @@
 <?php
-$servername = "ealybe55bg.database.windows.net,1433";
-$username = "opallity";
-$password = "Iltemp071993";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+// DB connection info
+$host = "ealybe55bg.database.windows.net,1433";
+$user = "opallity";
+$pwd = "Iltemp071993";
+$db = "OpallityFinal2";
+try{
+    $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    $sql = "CREATE TABLE registration_tbl(
+    id INT NOT NULL IDENTITY(1,1) 
+    PRIMARY KEY(id),
+    name VARCHAR(30),
+    email VARCHAR(30),
+    date DATE)";
+    $conn->query($sql);
 }
-echo "Connected successfully";
+catch(Exception $e){
+    die(print_r($e));
+}
+echo "<h3>Table created.</h3>";
 ?>
