a4dc2137670cb97e7b8ded690bac64d5e26aa9c7
Darien Pourmehr <darienpourmehr@gmail.com>
2015-10-18 04:31:58 -0400

    added db

--- response.php
+++ response.php
@@ -1,14 +1,14 @@
 <?php
 // DB connection info
-$host = "ealybe55bg.database.windows.net";
-$user = "opallity";
-$pwd = "Iltemp071993";
-$db = "OpallityFinal2";
+$host = "us-cdbr-azure-central-a.cloudapp.net";
+$user = "b2c629dc1ba64f";
+$pwd = "037a00b2";
+$db = "as_105e26c294c8255";
 $conn = mysqli_connect($host, $user, $pwd);
 
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo json_encode('yes');
+echo json_encode('yes')
 ?>
