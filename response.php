4cf1d086830087b6d765a5c344e2b03cf175fc5c
Darien Pourmehr <darienpourmehr@gmail.com>
2015-10-18 05:27:52 -0400

    added db

--- response.php
+++ response.php
@@ -7,14 +7,17 @@ $db = "as_105e26c294c8255";
 
 $mysqli = new mysqli($host,$user,$pwd,$db);
 
-$myArray = array();
-if ($result = $mysqli->query("SELECT * FROM Players")) {
+if(isset($_POST['username']) && isset($_POST['password'])) {
+	$myArray = array();
+	if ($result = $mysqli->query("SELECT * FROM Players")) {
 
-    while($row = $result->fetch_array(MYSQL_ASSOC)) {
-            $myArray[] = $row;
-    }
-    echo json_encode($myArray);
+	    while($row = $result->fetch_array(MYSQL_ASSOC)) {
+	            $myArray[] = $row;
+	    }
+	    echo json_encode($myArray);
+	}
 }
+
 
 /*if ($conn->query("INSERT INTO Players (username, password, firstName, lastName, email, zipcode) VALUES ('rsmith', '1111', 'Robert', 'Smith', 'frogger287@gmail.com', '30092')") === TRUE) {
     echo json_encode("Rob was entered into the database correctly.");
