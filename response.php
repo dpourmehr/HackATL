afcb6a903a599f10661d4038fc1d60d39b3ce278
Darien Pourmehr <darienpourmehr@gmail.com>
2015-10-18 05:04:21 -0400

    added db

--- response.php
+++ response.php
@@ -4,18 +4,19 @@ $host = "us-cdbr-azure-central-a.cloudapp.net";
 $user = "b2c629dc1ba64f";
 $pwd = "037a00b2";
 $db = "as_105e26c294c8255";
-$conn = new mysqli($host, $user, $pwd, $db);
+$conn = mysqli_connect($host, $user, $pwd, $db);
 
-if ($conn->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
 
-$result = $conn->query("select * from Players");
+if ($result = mysqli_query($conn, "SELECT Name FROM Players")) {
+    printf("Select returned %d rows.\n", mysqli_num_rows($result));
 
-echo json_encode($result);
-
-$result->close();
+    /* free result set */
+    mysqli_free_result($result);
+}
 
 /*if ($conn->query("INSERT INTO Players (username, password, firstName, lastName, email, zipcode) VALUES ('rsmith', '1111', 'Robert', 'Smith', 'frogger287@gmail.com', '30092')") === TRUE) {
     echo json_encode("Rob was entered into the database correctly.");
