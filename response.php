b8c39589679374cb7d4763d03f3b3afb45d95acd
Darien Pourmehr <darienpourmehr@gmail.com>
2015-10-18 06:34:55 -0400

    added db

--- response.php
+++ response.php
@@ -9,7 +9,7 @@ $mysqli = new mysqli($host,$user,$pwd,$db);
 
 if(isset($_POST['username']) && isset($_POST['password'])) {
 	$myArray = array();
-	if ($result = $mysqli->query("ALTER TABLE Players DROP COLUMN opalls") {
+	if ($result = $mysqli->query("INSERT INTO Players (username, password, firstName, lastName, email, zipcode) VALUES ('rsmith', '1111', 'Robert', 'Smith', 'frogger287@gmail.com', '30092')")) {
 		echo "yes";
 	}
 }
