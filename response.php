26d6d61e46f5e09ef3547647ab97aaa3c2fb333d
Darien Pourmehr <darienpourmehr@gmail.com>
2015-10-18 02:15:59 -0400

    added header

--- response.php
+++ response.php
@@ -5,7 +5,7 @@ $user = "opallity";
 $pwd = "Iltemp071993";
 $db = "OpallityFinal2";
 try{
-    $conn = new PDO( "sqlsrv:Server= '$host' ; Database = '$db' ", $user, $pwd);
+    $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     $sql = "CREATE TABLE registration_tbl(
     id INT NOT NULL IDENTITY(1,1) 
@@ -19,4 +19,5 @@ catch(Exception $e){
     die(print_r($e));
 }
 echo "<h3>Table created.</h3>";
+try {$conn = new PDO ( \"sqlsrv:server = tcp:ealybe55bg.database.windows.net,1433; Database = OpallityFinal2\", \"opallity\", \"{Iltemp071993}\");\r\n    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION ");} catch ( PDOException $e ) {print( "Error connecting to SQL Server." );   die(print_r($e));}
 ?>
