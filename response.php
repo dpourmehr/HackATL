afee7971bf2898c16d69e0a5c0c9d263f3cf6ee8
Darien Pourmehr <darienpourmehr@gmail.com>
2015-10-18 04:56:53 -0400

    added db

--- response.php
+++ response.php
@@ -6,12 +6,12 @@ $pwd = "037a00b2";
 $db = "as_105e26c294c8255";
 $conn = new mysqli($host, $user, $pwd, $db);
 
-if ($mysqli->connect_errno) {
+if ($conn->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
 
-if ($mysqli->query("CREATE TABLE Players (username varchar(255), password varchar(255), firstName varchar(255), lastName varchar(255), email varchar(255), zipcode varchar(255))") === TRUE) {
+if ($conn->query("CREATE TABLE Players (username varchar(255), password varchar(255), firstName varchar(255), lastName varchar(255), email varchar(255), zipcode varchar(255))") === TRUE) {
     echo json_encode("Table myCity successfully created.");
 }
 
