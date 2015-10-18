461202c6d482bc1c953af2ddfe8459eb927bf1e5
Darien Pourmehr <darienpourmehr@gmail.com>
2015-10-18 04:52:25 -0400

    added db

--- response.php
+++ response.php
@@ -4,12 +4,10 @@ $host = "us-cdbr-azure-central-a.cloudapp.net";
 $user = "b2c629dc1ba64f";
 $pwd = "037a00b2";
 $db = "as_105e26c294c8255";
-$conn = mysqli_connect($host, $user, $pwd);
+$conn = new mysqli($host, $user, $pwd, $db);
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
 }
-$result = mysqli_query($conn,"CREATE TABLE Players (username varchar(255), password varchar(255), firstName varchar(255), lastName varchar(255), email varchar(255), zipcode varchar(255))");
-echo json_encode($result);
 ?>
